50270bc11c6a3d2ac9578226def32b54224a7786
Henrick Tambourou <h.tambourou@gmail.com>
2021-11-15 10:06:18 -0600

    practice probs

---
+++ Practice/container_with_most_water.js
@@ -0,0 +1,19 @@
+const height = [1, 7, 4, 6, 9, 8, 7];
+
+function maxArea(in1){
+    let leftBound = 0;
+    let rightBound = in1.length - 1;
+    let max = 0;
+    while(leftBound < rightBound){
+        if(in1[leftBound] < in1[rightBound]){
+            max = Math.max(max, (rightBound - leftBound)*in1[leftBound]);
+            leftBound++
+        }
+        else{
+            max = Math.max(max, (rightBound - leftBound)*in1[rightBound]);
+            rightBound--;
+        }
+    }
+    return max; 
+}
+//maxArea(height) -> 35
