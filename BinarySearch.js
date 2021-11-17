8036798325e83077d5e81a46f1fcfa36f237d69a
Henrick Tambourou <h.tambourou@gmail.com>
2021-11-17 13:45:38 -0600

    concepts

---
+++ BinarySearch.js
@@ -0,0 +1,27 @@
+//BinarySearch (Divide & Conquer)
+// - O(log N)
+
+let x = 70; 
+const arr = [1, 5, 6, 9, 15, 20, 35, 34, 50, 70, 89, 90, 99, 100, 101];
+
+function binarySearch(x, arr){
+    let start =0;
+    let end = array.length -1;
+    
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (array[middle] === num) {
+            // found the key
+            return "the number was found";
+        } else if (array[middle] < num) {
+            // continue searching to the right
+            start = middle + 1;
+        } else {
+            // search searching to the left
+            end = middle - 1;
+        }
+    }
+	// key wasn't found
+    return -1
+}
