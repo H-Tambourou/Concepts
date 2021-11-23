37ad14f5e1622a5c5c4d2e4e8fa1067daf00589d
Henrick Tambourou <h.tambourou@gmail.com>
2021-11-22 23:00:32 -0600

    concept

--- BigO.js
+++ BigO.js
@@ -1,5 +1,6 @@
 let arr1= [1,2,3,4];
 let arr2=[1,2,3,4];
+let match= 3;
 
 function printPairs(arr1, arr2){
     for(let i=0; i < arr1.length; i++){
@@ -16,3 +17,11 @@ function printPairs(arr1){
         }
     }
 } //Runtime is O(N^2)
+
+
+function findIndex(arr1, match){
+    for(let i =0; i < arr1.length; i++){
+        if(arr1[i] == match) {return i};
+        return -1;
+    }
+}// Runtime is O(N)
