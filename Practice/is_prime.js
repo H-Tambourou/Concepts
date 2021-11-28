b0925ef5fd3536649aa794672d7bc84f0dd51341
Henrick Tambourou <h.tambourou@gmail.com>
2021-11-27 23:12:53 -0600

    practice probs

---
+++ Practice/is_prime.js
@@ -0,0 +1,14 @@
+let num = 11;
+
+const isPrime = (i) =>{
+    let divisor = 2;
+    while(i> divisor){
+        if(n % divisor== 0){
+            return false;
+        }
+        else{
+            divisor++;
+        }
+        return true;
+    }
+}//isPrime(num) -> true
