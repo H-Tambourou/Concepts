75354f4088e2bfea89d136e7df6a1c08d44cd4e1
Henrick Tambourou <h.tambourou@gmail.com>
2021-11-28 23:57:59 -0600

    concepts

--- hashTable.js
+++ hashTable.js
@@ -3,3 +3,12 @@ let obj = {
     Jane: "315-0322"
   }
   //JavaScript object is an example of Hash Table implementation
+
+  const collection = new Map();
+
+collection.set("Nathan", "555-0182");
+collection.set("Jane", "555-0182");
+
+console.log(collection.get("Nathan")); // 555-0182
+console.log(collection.size); // 2
+//JavaScript Map class is another implementation of Hash Table
