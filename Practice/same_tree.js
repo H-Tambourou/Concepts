82ef86b358891fbc35f27ffeb7a5302dc7e3e82d
Henrick Tambourou <h.tambourou@gmail.com>
2022-03-29 18:24:05 -0500

    practice probs

---
+++ Practice/same_tree.js
@@ -0,0 +1,5 @@
+const isSameTree = function(p, q) {
+    if(!p || !q) return p == q;
+    if(p.val == q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    return false;
+};
