45a7d4df3bbb85adf44f33a072abfcc469ef1747
Henrick Tambourou <h.tambourou@gmail.com>
2022-04-05 22:36:19 -0500

    practice probs

---
+++ Practice/jump_game.js
@@ -0,0 +1,11 @@
+const canJump = function(nums) {
+    if(nums.length<= 1) return true;
+    let max = 0; 
+    
+    for(let i=0; i < nums.length - 1; i++){
+        max = Math.max(max, nums[i] + i);
+        
+        if(max<= i) return false;
+        if(max>= nums.length - 1) return true
+    }
+};
